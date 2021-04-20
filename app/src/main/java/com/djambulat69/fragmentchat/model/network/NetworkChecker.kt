cbe30e493ef122a62889f605941e3ecf3a7bb4db
Djambulat <Djambulat69@gmail.com>
2021-04-20 16:12:48 +0300

    fixed connectivityManager leak

--- app/src/main/java/com/djambulat69/fragmentchat/model/network/NetworkChecker.kt
+++ app/src/main/java/com/djambulat69/fragmentchat/model/network/NetworkChecker.kt
@@ -8,15 +8,17 @@ import com.djambulat69.fragmentchat.ui.FragmentChatApplication
 
 object NetworkChecker {
 
-    private val connectivityManager =
-        FragmentChatApplication.applicationContext().getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
-    private val networkRequest = NetworkRequest.Builder().build()
 
     fun registerNetworkCallback(callback: ConnectivityManager.NetworkCallback) {
+        val connectivityManager =
+            FragmentChatApplication.applicationContext().getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        val networkRequest = NetworkRequest.Builder().build()
         connectivityManager.registerNetworkCallback(networkRequest, callback)
     }
 
     fun isConnected(): Boolean {
+        val connectivityManager =
+            FragmentChatApplication.applicationContext().getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
         for (network in connectivityManager.allNetworks) {
             val capabilities = connectivityManager.getNetworkCapabilities(network)
             if (capabilities?.hasCapability(NetworkCapabilities.NET_CAPABILITY_INTERNET) == true) {
