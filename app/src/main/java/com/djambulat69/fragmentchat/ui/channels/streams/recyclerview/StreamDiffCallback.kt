ac6ccba08698afdb9ee187dbbd307bc9b50e3088
Djambulat <Djambulat69@gmail.com>
2021-04-25 17:46:57 +0300

    fixed bug in StreamDiffCallback.kt

--- app/src/main/java/com/djambulat69/fragmentchat/ui/channels/streams/recyclerview/StreamDiffCallback.kt
+++ app/src/main/java/com/djambulat69/fragmentchat/ui/channels/streams/recyclerview/StreamDiffCallback.kt
@@ -11,7 +11,7 @@ object StreamDiffCallback : DiffUtil.ItemCallback<ViewTyped>() {
 
     override fun areContentsTheSame(oldItem: ViewTyped, newItem: ViewTyped): Boolean {
         return when {
-            oldItem is StreamUI && newItem is StreamUI -> false
+            oldItem is StreamUI && newItem is StreamUI -> oldItem as StreamUI == newItem as StreamUI && oldItem.isExpanded == newItem.isExpanded
             oldItem is TopicUI && newItem is TopicUI -> oldItem.topic == newItem.topic
             else -> true
         }
